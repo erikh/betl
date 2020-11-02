3485537525119f513430a44d6ac7af6fe61eae9b
Erik Hollensbe <github@hollensbe.org>
2020-11-01 16:19:41 -0800

    add material ui and start doing design work
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { React } from "react";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography>Please enter a blog post URL</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <form method="POST" action="/url">
+            <FormControl>
+              <InputLabel htmlFor="url">URL</InputLabel>
+              <Input id="url" name="url" aria-describedby="url-description" />
+              <FormHelperText id="url-description">
+                Put your blog's URL here.
+              </FormHelperText>
+            </FormControl>
+          </form>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
